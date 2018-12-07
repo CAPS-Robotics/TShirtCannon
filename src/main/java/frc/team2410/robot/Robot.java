2999d475c6b2b387d0e60f2b2dd5b4056b6d8df9
HiItsMeHaHa <wherron01@gmail.com>
2018-12-07 14:59:17 -0600

    init

---
+++ src/main/java/frc/team2410/robot/Robot.java
@@ -0,0 +1,39 @@
+package frc.team2410.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot
+{
+
+  public Robot() {}
+
+  public void robotInit() {
+
+  }
+
+  public void disabledInit() {}
+
+  public void disabledPeriodic() {
+
+  }
+
+  public void autonomousInit() {
+
+  }
+
+  public void autonomousPeriodic() {
+
+  }
+
+  public void teleopInit() {
+
+  }
+
+  public void teleopPeriodic() {
+
+  }
+
+  public void testPeriodic() {}
+}
